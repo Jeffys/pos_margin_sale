9a1ba0a34bf2c16e224c9b4fb089045024317c4f
cherzs <zhafrang638@gmail.com>
2024-10-07 09:21:53 +0700

    cek

--- pos_margin_sale/wizard/sale_confirmation.py
+++ pos_margin_sale/wizard/sale_confirmation.py
@@ -1,19 +1,19 @@
-from odoo import _, api, fields, models
+# from odoo import _, api, fields, models
 
-class SaleConfirmationWizard(models.TransientModel):
-    _name = 'sale.confirmation.wizard'
-    _description = "Sale confirmation wizard"
+# class SaleConfirmationWizard(models.TransientModel):
+#     _name = 'sale.confirmation.wizard'
+#     _description = "Sale confirmation wizard"
 
-    message = fields.Text(string="Message", translate=True)
+#     message = fields.Text(string="Message", translate=True)
 
-    def action_confirms(self):
-        active_model = self._context.get('active_model')
-        active_id = self._context.get('active_ids')
-        if active_model == 'sale.order':
-            sale = self.env[active_model].browse(active_id)
+#     def action_confirms(self):
+#         active_model = self._context.get('active_model')
+#         active_id = self._context.get('active_ids')
+#         if active_model == 'sale.order':
+#             sale = self.env[active_model].browse(active_id)
 
-            # Check if the sale order is a rental order
-            if sale.is_rental_order:
-                return  # Skip the confirmation for rental orders
+#             # Check if the sale order is a rental order
+#             if sale.is_rental_order:
+#                 return  # Skip the confirmation for rental orders
 
-            sale.with_context(skip_check_price=True).action_confirms()
+#             sale.with_context(skip_check_price=True).action_confirms()
