4df7e8927e787a41c173534e3edcc9b36a2e43bc
cherzs <zhafrang638@gmail.com>
2024-10-07 16:32:30 +0700

    Trial

--- pos_margin_sale/wizard/sale_confirmation.py
+++ pos_margin_sale/wizard/sale_confirmation.py
@@ -1,19 +1,34 @@
-# from odoo import _, api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
-# class SaleConfirmationWizard(models.TransientModel):
-#     _name = 'sale.confirmation.wizard'
-#     _description = "Sale confirmation wizard"
+class SaleConfirmationWizard(models.TransientModel):
+    _name = 'sale.confirmation.wizard'
+    _description = "Sale confirmation wizard"
 
-#     message = fields.Text(string="Message", translate=True)
+    message = fields.Text(string="Message", translate=True)
 
-#     def action_confirms(self):
-#         active_model = self._context.get('active_model')
-#         active_id = self._context.get('active_ids')
-#         if active_model == 'sale.order':
-#             sale = self.env[active_model].browse(active_id)
+    def action_confirms(self):
+        active_model = self._context.get('active_model')
+        active_ids = self._context.get('active_ids', [])
+        
+        if active_model == 'sale.order':
+            sale = self.env[active_model].browse(active_ids)
 
-#             # Check if the sale order is a rental order
-#             if sale.is_rental_order:
-#                 return  # Skip the confirmation for rental orders
+            if sale.is_rental_order:
+                # Directly confirm the sale if it's a rental order
+                return sale.action_confirm()
 
-#             sale.with_context(skip_check_price=True).action_confirms()
+            # For non-rental orders, check minimum sale price
+            products_below_minimum = sale.check_product_price()
+            if products_below_minimum:
+                # Handle the case where the products are below minimum price
+                product_str = '\n'.join(
+                    f"{i + 1}. {product.display_name} minimum price is {product.currency_id.symbol} {product.minimum_sale_price:.2f}"
+                    for i, product in enumerate(products_below_minimum)
+                )
+                raise ValidationError(_(
+                    f"The price of this product is lower than the minimum sale price:\n{product_str}"
+                ))
+
+            # Confirm the sale order if all checks pass
+            sale.with_context(skip_check_price=True).action_confirm()
