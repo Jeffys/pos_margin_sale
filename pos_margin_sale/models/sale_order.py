6a12a5a5a3c7e725503c6659d6c2ae973ee0c30b
cherzs <zhafrang638@gmail.com>
2024-10-08 08:35:21 +0700

    back first

--- pos_margin_sale/models/sale_order.py
+++ pos_margin_sale/models/sale_order.py
@@ -1,48 +1,33 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
+
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def action_confirm(self):
-        # Check if the order is a rental order
-        if self.is_rental_order:
-            return super(SaleOrder, self).action_confirm()
 
-        # Continue with minimum price validation for non-rental orders
-        skip_check_price = self.env.context.get('skip_check_price')
+    def action_confirm(self):
+        skip_check_price = self._context.get('skip_check_price')
         check_product = self.check_product_price()
         blocking_warning = self.env['ir.config_parameter'].sudo().get_param('post_margin_sale.blocking_transaction_order')
-
         if len(check_product) > 0 and not skip_check_price:
-            product_str = ('\n').join(
-                f" {i + 1}. {product.display_name} minimum price is {product.currency_id.symbol} {product.minimum_sale_price:.2f}"
-                for i, product in enumerate(check_product)
-            )
-            product_str_fr = ('\n').join(
-                f" {i + 1}. {product.display_name} le prix minimum est {product.currency_id.symbol} {product.minimum_sale_price:.2f}"
-                for i, product in enumerate(check_product)
-            )
-
-            message = _(f"Price of this product is less than minimum sale price \n\n{product_str}")
-            message_fr = f"Le prix de ce produit est inférieur au prix de vente minimum \n\n{product_str_fr}"
+            product_str = ('\n').join(f" {i + 1}. {product.display_name} minimum price is {product.currency_id.symbol}. {product.minimum_sale_price:.2f}" for i,product in enumerate(check_product))
+            product_str_fr = ('\n').join(f" {i + 1}. {product.display_name} le prix minimum est {product.currency_id.symbol}. {product.minimum_sale_price:.2f}" for i,product in enumerate(check_product))
+            message = (_(f"Price of this product is less than minimum sale price \n\n{product_str}"))
+            message_Fr = f"Le prix de ce produit est inférieur au prix de vente minimum \n\n{product_str_fr}"
             user_language = self.detect_user_language()
-
-            # if blocking_warning:
-            #     if user_language == 'French':
-            #         raise ValidationError(_(f"{message_fr} \n\nTransaction bloquée car prix inférieur au prix minimum de vente."))
-            #     else:
-            #         raise ValidationError(_(f"{message} \n\nTransaction blocked due to price being lower than the minimum sale price."))
-            # else:
+            if blocking_warning:
+                    if user_language == 'French':
+                        raise ValidationError(_(f"{message_Fr} \n\nTransaction bloquée car prix inférieur au prix minimum de vente."))
+                    else:
+                        raise ValidationError(_(f"{message} \n\nTransaction blocked due to price being lower than the minimum sale price."))
+            else:
                 message += "\n\nDo you want to continue with the quotation for making sale order?"
-                message_fr += "\n\nVoulez-vous continuer avec le devis pour passer commande ?"
-                
-                # Create the wizard with both English and French messages
+                message_Fr += "\n\nVoulez-vous continuer avec le devis pour passer commande ?"
                 wizard = self.env['sale.confirmation.wizard'].create({'message': message})
                 wizard.with_context(lang='fr_FR').write({
-                    'message': message_fr
-                })
-                
+                'message': message_Fr})
                 return {
                     'type': 'ir.actions.act_window',
                     'name': _('Confirm minimum sale price'),
@@ -51,7 +36,6 @@ class SaleOrder(models.Model):
                     'target': 'new',
                     'res_id': wizard.id,
                 }
-        
         return super(SaleOrder, self).action_confirm()
 
     def detect_user_language(self):
@@ -67,13 +51,12 @@ class SaleOrder(models.Model):
     def check_product_price(self):
         products = []
         for line in self.order_line:
-            # Check if line price is less than the product's minimum sale price
             if line.price_unit < line.minimum_sale_price:
                 products.append(line.product_id)
         return products
 
-
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
 
     minimum_sale_price = fields.Float(string="Minimum sale price", related='product_id.minimum_sale_price')
